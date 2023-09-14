8a4ed84bd0cb202e493bbd949edeac8d2e03b913
Davitpurceladze <purceladzedavit@gmail.com>
2023-09-14 19:03:47 +0400

    add hover and top line effect to navbar

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -32,4 +32,31 @@ const DivStyled = styled.div`
   line-height: 25px;
   letter-spacing: 1px;
   font-weight: 700;
+  opacity: 75%;
+  text-align: center;
+  transition: 0.3s;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: -30px;
+    left: 0;
+    right: 0;
+    height: 4px;
+    background-color: #419ebb;
+    opacity: 0;
+    transition: opacity 0.3s;
+    border-radius: 3px;
+  }
+
+  &:hover::before {
+    opacity: 1;
+  }
+
+  &:hover {
+    opacity: 100%;
+    transition: opacity 0.3s;
+    cursor: pointer;
+  }
 `;
